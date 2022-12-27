eba1431272f21e945225d13b5b7c8131952fbc8f
Rahul singh <rahulsingh19390@gmail.com>
2022-12-27 16:37:13 +0530

    Added Ideas Page

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Ideas() {
+  return (
+    <div>
+      Ideas for Bog website.
+      Login and signup Form.
+        User profile page.
+        User profile edit page.
+        
+    </div>
+  )
+}
+
+export default Ideas
