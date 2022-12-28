9a5680e0d64e197754fdd455e3161b896116c3ea
Rahul singh <rahulsingh19390@gmail.com>
2022-12-28 18:47:41 +0530

    Added assets

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Logo from '../img/logo.png'
 
 export default function Navbar() {
   return (
     <div className='navbar'>
       <div className="container">
-        <div className="logo">Logo</div>
+        <div className="logo">
+          <img src={Logo} alt="" />
+        </div>
         <div className="links">Links</div>
       </div>
     </div>
