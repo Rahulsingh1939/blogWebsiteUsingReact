0e5ade665c06d53aa7db6e1ab7a190375b3674fb
Rahul singh <rahulsingh19390@gmail.com>
2022-12-30 15:03:27 +0530

    Updated Logo img

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Logo from '../img/logo.png'
 
 export default function Navbar() {
@@ -8,7 +9,28 @@ export default function Navbar() {
         <div className="logo">
           <img src={Logo} alt="" />
         </div>
-        <div className="links">Links</div>
+        <div className="links">
+          <Link className='link' to="/?cat=art">
+            <h6>ART</h6>
+          </Link>
+          <Link className='link' to="/?cat=tech">
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link className='link' to="/?cat=cinema">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link className='link' to="/?cat=design">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className='link' to="/?cat=food">
+            <h6>FOOD</h6>
+          </Link>
+          <span>Rahul</span>
+          <span>Logout</span>
+          <span className="write">
+          <Link to="/write">Write</Link>
+          </span>
+        </div>
       </div>
     </div>
   )
