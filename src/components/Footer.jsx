807c201778f97c5907adea729ac5194777de9563
Rahul singh <rahulsingh19390@gmail.com>
2023-01-05 11:28:46 +0530

    Started working on Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Logo from '../img/logo.png'
 
 function Footer() {
   return (
     <footer>
+      <img src={Logo} alt="like" />
       <span className="footer-text">
         Made with ♥ and <b>React JS.</b>
       </span> 
